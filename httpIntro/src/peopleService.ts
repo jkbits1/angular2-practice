23bec475837ca55224208bf384572311d5e9422e
jkbits1 <jkbits1@users.noreply.github.com>
2015-11-27 04:13:16 +0000

    initial state

--- httpIntro/src/peopleService.ts
+++ httpIntro/src/peopleService.ts
@@ -2,4 +2,16 @@
  * Created by Jon on 27/11/15.
  */
 
+//a simple service
+import {Injectable} from 'angular2/angular2';
 
+@Injectable()
+export class PeopleService {
+    constructor(){
+        this.people = [
+            {"id": 1, "name": "Brad"},
+            {"id": 2, "name": "Jules"},
+            {"id": 3, "name": "Jeff"}
+        ];
+    }
+}
