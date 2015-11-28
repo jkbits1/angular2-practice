039847fff3211b7d99c8978f06335d0cce405805
jkbits1 <jkbits1@users.noreply.github.com>
2015-11-28 04:08:56 +0000

    basic rxjs example - completed

--- ang-rxjs/app.js
+++ ang-rxjs/app.js
@@ -11,12 +11,23 @@ app.controller('MainCtrl', function ($scope, rx) {
   var observable =
     rx.Observable
       .interval(5000)
+      .take(3)
       .safeApply($scope, function (x) {
         $scope.counter = x;
       });
 
   $scope.click = function () {
     $scope.buttonEnabled = false;
-    observable.subscribe();
+    observable.subscribe(
+      function onNext(val) {
+        console.log(val);
+      },
+      function onError (err) {
+        console.log(err);
+      },
+      function onCompleted () {
+        console.log('done');
+      }
+    );
   };
 });
