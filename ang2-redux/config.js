93f1d23f6b58a6863ce126ad5084438597bc7300
jkbits1 <jkbits1@users.noreply.github.com>
2016-02-27 21:11:36 +0000

    basic redux app

--- ang2-redux/config.js
+++ ang2-redux/config.js
@@ -1,3 +1,20 @@
 /**
  * Created by Jon on 27/02/16.
  */
+
+System.config({
+  transpiler: 'typescript',
+  typescriptOptions: {
+    emitDecoratorMetadata: true
+  },
+  map: {
+    app: "./src",
+    'redux': 'https://cdnjs.cloudflare.com/ajax/libs/redux/3.0.4/redux.js'
+  },
+  packages: {
+    app: {
+      main: './main.ts',
+      defaultExtension: 'ts'
+    }
+  }
+});
